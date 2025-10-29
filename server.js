ba6acb9100877cc59833e7142e30b84822a1e141
MIOjp <miotikusyo@gmail.com>
2025-10-29 12:42:55 +0900

    a

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 app.use(express.static("public"));
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3003
 const sentiment = new Sentiment();
 
 // --- Spotifyトークン取得 ---
