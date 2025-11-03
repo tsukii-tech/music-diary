838d28783d26e926fada09516d18a4c65572c0cb
MIOjp <miotikusyo@gmail.com>
2025-11-03 20:18:33 +0900

    ようやくエラーがなくなって読み込めた！

---
+++ music-diary/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main style={{ textAlign: "center", paddingTop: "100px" }}>
+      <h1>🎵 My Music Diary 🎶</h1>
+      <p>Next.js がちゃんと動いてるよ！</p>
+      <p>ここに日記アプリを作っていこう✨</p>
+    </main>
+  );
+}
