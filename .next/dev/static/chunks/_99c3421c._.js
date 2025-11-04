c973387e054dec1d9ff66f6baae5ef5d0299790b
MIOjp <miotikusyo@gmail.com>
2025-11-04 11:42:32 +0900

    1142

--- .next/dev/static/chunks/_99c3421c._.js
+++ .next/dev/static/chunks/_99c3421c._.js
@@ -8,9 +8,11 @@ __turbopack_context__.s([
 ]);
 var __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$compiled$2f$react$2f$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__ = __turbopack_context__.i("[project]/node_modules/next/dist/compiled/react/jsx-dev-runtime.js [app-client] (ecmascript)");
 var __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$navigation$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__ = __turbopack_context__.i("[project]/node_modules/next/navigation.js [app-client] (ecmascript)");
+var __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$compiled$2f$react$2f$index$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__ = __turbopack_context__.i("[project]/node_modules/next/dist/compiled/react/index.js [app-client] (ecmascript)");
 ;
 var _s = __turbopack_context__.k.signature();
 "use client";
+;
 ;
 ;
 function DiaryPage() {
--- app/diary/page.jsx
+++ app/diary/page.jsx
@@ -1,7 +1,7 @@
 "use client";
-import { useSearchParams } from "next/navigation";
 import { useRouter } from "next/navigation";
-
+import { useSearchParams } from "next/navigation";
+import { useEffect } from "react";
 
 export default function DiaryPage() {
   const params = useSearchParams();
