5e00642d270eb7751a3b5ce22998e4278ecf184d
tsukii-tech <tsukiha.s.star@gmail.com>
2025-11-04 20:26:56 +0900

    11/4更新

--- app/diarylist/page.tsx
+++ app/diarylist/page.tsx
@@ -2,24 +2,9 @@
 import { useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
 
-<<<<<<< Updated upstream
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-export default function DiaryPage() {
+export default function DiaryListPage() {
     const searchParams = useSearchParams();
     const [text, setText] = useState("");
-=======
-=======
->>>>>>> 9e1583be85268a66c15e3960c2d40e3f3febc898
-=======
->>>>>>> 9e1583be85268a66c15e3960c2d40e3f3febc898
->>>>>>> Stashed changes
-
-export default function DiaryListPage() {
-    const [diaries, setDiaries] = useState<any[]>([]);
->>>>>>> 9e1583be85268a66c15e3960c2d40e3f3febc898
 
     useEffect(() => {
         setText(searchParams.get("text") || "");
